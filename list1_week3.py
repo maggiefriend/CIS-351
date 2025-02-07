3464c02389e87e16cac03d13a1a67561fcaa3ac5
maggiefriend <156940616+maggiefriend@users.noreply.github.com>
2025-02-07 12:10:04 -0500

    Add files via upload

---
+++ list1_week3.py
@@ -0,0 +1,54 @@
+# A. match_ends
+def match_ends(words):
+    count = 0
+    for word in words:
+        if len(word) >= 2 and word[0] == word[-1]:
+            count += 1
+    return count
+
+# B. front_x
+def front_x(words):
+    x_list = [word for word in words if word.startswith('x')]
+    non_x_list = [word for word in words if not word.startswith('x')]
+    return sorted(x_list) + sorted(non_x_list)
+
+# C. sort_last
+def sort_last(tuples):
+    return sorted(tuples, key=lambda x: x[-1])
+
+# Simple provided test() function used in main() to print
+# what each function returns vs. what it's supposed to return.
+def test(got, expected):
+    if got == expected:
+        prefix = ' OK '
+    else:
+        prefix = '  X '
+    print('%s got: %s expected: %s' % (prefix, repr(got), repr(expected)))
+
+# Calls the above functions with interesting inputs.
+def main():
+    print('match_ends')
+    test(match_ends(['aba', 'xyz', 'aa', 'x', 'bbb']), 3)
+    test(match_ends(['', 'x', 'xy', 'xyx', 'xx']), 2)
+    test(match_ends(['aaa', 'be', 'abc', 'hello']), 1)
+
+    print()
+    print('front_x')
+    test(front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa']),
+         ['xaa', 'xzz', 'axx', 'bbb', 'ccc'])
+    test(front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa']),
+         ['xaa', 'xcc', 'aaa', 'bbb', 'ccc'])
+    test(front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark']),
+         ['xanadu', 'xyz', 'aardvark', 'apple', 'mix'])
+
+    print()
+    print('sort_last')
+    test(sort_last([(1, 3), (3, 2), (2, 1)]),
+         [(2, 1), (3, 2), (1, 3)])
+    test(sort_last([(2, 3), (1, 2), (3, 1)]),
+         [(3, 1), (1, 2), (2, 3)])
+    test(sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)]),
+         [(2, 2), (1, 3), (3, 4, 5), (1, 7)])
+
+if __name__ == '__main__':
+    main()
---
+++ string1_week3.py
@@ -0,0 +1,65 @@
+
+# A. donuts
+def donuts(count):
+    if count >= 10:
+        return 'Number of donuts: many'
+    else:
+        return f'Number of donuts: {count}'
+
+# B. both_ends
+def both_ends(s):
+    if len(s) < 2:
+        return ''
+    return s[:2] + s[-2:]
+
+# C. fix_start
+def fix_start(s):
+    first_char = s[0]
+    return first_char + s[1:].replace(first_char, '*')
+
+# D. mix_up
+def mix_up(a, b):
+    return b[:2] + a[2:] + ' ' + a[:2] + b[2:]
+
+# Provided simple test() function used in main() to print
+# what each function returns vs. what it's supposed to return.
+def test(got, expected):
+    if got == expected:
+        prefix = ' OK '
+    else:
+        prefix = '  X '
+    print('%s got: %s expected: %s' % (prefix, repr(got), repr(expected)))
+
+# Provided main() calls the above functions with interesting inputs,
+# using test() to check if each result is correct or not.
+def main():
+    print('donuts')
+    test(donuts(4), 'Number of donuts: 4')
+    test(donuts(9), 'Number of donuts: 9')
+    test(donuts(10), 'Number of donuts: many')
+    test(donuts(99), 'Number of donuts: many')
+
+    print()
+    print('both_ends')
+    test(both_ends('spring'), 'spng')
+    test(both_ends('Hello'), 'Helo')
+    test(both_ends('a'), '')
+    test(both_ends('xyz'), 'xyyz')
+
+    print()
+    print('fix_start')
+    test(fix_start('babble'), 'ba**le')
+    test(fix_start('aardvark'), 'a*rdv*rk')
+    test(fix_start('google'), 'goo*le')
+    test(fix_start('donut'), 'donut')
+
+    print()
+    print('mix_up')
+    test(mix_up('mix', 'pod'), 'pox mid')
+    test(mix_up('dog', 'dinner'), 'dig donner')
+    test(mix_up('gnash', 'sport'), 'spash gnort')
+    test(mix_up('pezzy', 'firm'), 'fizzy perm')
+
+# Standard boilerplate to call the main() function.
+if __name__ == '__main__':
+    main()
